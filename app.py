41a2eddad7697ca369e9fe1a58b269faca369aaf
Edcorlo <edlopz@hotmail.es>
2026-07-31 13:27:33 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from nltk.sentiment import SentimentIntensityAnalyzer
 # --- PAGE CONFIGURATION ---
 st.set_page_config(
     page_title="E-Commerce Sentiment Engine (Unlimited CSV)",
-    page_icon="💡",
+    page_icon="🔥🗣️",
     layout="wide"
 )
 
